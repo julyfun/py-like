622d66af31169b15175419d41ffb4cd67b21a154
Julyfun macOS13.4 M1 <43675484+julyfun@users.noreply.github.com>
2024-04-01 12:49:11 +0800

    feat: 修改 type_of 的返回类型为 &'static str

--- src/types.rs
+++ src/types.rs
@@ -1,14 +1,18 @@
+/// Use a variable to get its type name (instead of using the type).
+///
+/// # Python Counterpart
+///
 /// ```py
 /// type(a)
 /// ```
 ///
-/// # Usage
+/// # Examples
 ///
 /// ```rust
 /// use py_like::type_of; // This is even a test..
 /// let a = 1.0;
 /// println!("{}", type_of(&a));
 /// ```
-pub fn type_of<T>(_: &T) -> String {
-    format!("{}", std::any::type_name::<T>())
+pub fn type_of<T>(_: &T) -> &'static str {
+    std::any::type_name::<T>()
 }
